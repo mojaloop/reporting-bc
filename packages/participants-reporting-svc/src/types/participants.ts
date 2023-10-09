6df2d5311f48d3d4872bc6d8677fecb1fbf1d55d
kyawhein <myo.htet@thitsaworks.com>
2023-10-09 12:28:38 +0630

    3568 Add participants-reporting-svc in the reporting-bc

---
+++ packages/participants-reporting-svc/src/types/participants.ts
@@ -0,0 +1,72 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * ThitsaWorks
+ - Myo Min Htet <myo.htet@thitsaworks.com>
+
+ --------------
+******/
+"use strict";
+
+import {
+    IParticipantAllowedSourceIp,
+    IParticipantEndpoint,
+    IParticipantAccount,
+    IParticipantFundsMovement,
+    IParticipantNetDebitCap,
+    IParticipantAccountChangeRequest,
+    IParticipantActivityLogEntry,
+    IParticipantNetDebitCapChangeRequest
+} from "@mojaloop/participant-bc-public-types-lib";
+
+export declare interface IParticipant {
+    id: string;
+  name: string;
+  type: "HUB" | "DFSP";
+  isActive: boolean;
+  description: string;
+
+  createdBy: string;
+  createdDate: number;
+
+  approved: boolean;
+  approvedBy: string | null;
+  approvedDate: number | null;
+
+  lastUpdated: number;
+
+  participantAllowedSourceIps: IParticipantAllowedSourceIp[];
+  participantEndpoints: IParticipantEndpoint[];
+  participantAccounts: IParticipantAccount[];
+  participantAccountsChangeRequest: IParticipantAccountChangeRequest[];
+
+  fundsMovements: IParticipantFundsMovement[];
+  changeLog: IParticipantActivityLogEntry[];
+
+  // only per currency
+  netDebitCaps: IParticipantNetDebitCap[];
+  netDebitCapChangeRequests: IParticipantNetDebitCapChangeRequest[];
+  }
+  
+  
